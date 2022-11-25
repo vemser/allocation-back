a34cd95b58cc23a51b430a5b38eb13592a652d25
KaioAntonio <kaioaar@gmail.com>
2022-11-25 17:45:24 -0300

    add: projeto inicial spring

---
+++ src/main/java/com/allocation/AllocationApplication.java
@@ -0,0 +1,13 @@
+package com.allocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllocationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AllocationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/allocation/AllocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allocation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
