5917e450fe95424cfa30c7ef37d20833f34813e3
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2022-11-25 21:59:52 -0300

    add UsuarioEntity

--- src/main/java/com/allocation/entity/UsuarioEntity.java
+++ src/main/java/com/allocation/entity/UsuarioEntity.java
@@ -1,4 +1,29 @@
 package com.allocation.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity(name = "Usuario")
 public class UsuarioEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_USUARIO")
+    @SequenceGenerator(name = "SEQ_USUARIO", sequenceName = "seq_usuario", allocationSize = 1)
+    @Column(name = "ID_USUARIO")
+    private int idUsuario;
+
+    @Column(name = "nome_completo")
+    private String nomeCompleto;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Column(name = "foto")
+    private String foto;
+
 }
