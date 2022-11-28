11bf92336167e4c41fddaaa987709c2c63a410c4
Gustavo Lucena <gustavolucena12@gmail.com>
2022-11-27 21:29:45 -0300

    add: vaga controller

---
+++ src/main/java/br/com/allocation/controller/VagaController.java
@@ -0,0 +1,55 @@
+package br.com.allocation.controller;
+
+import br.com.allocation.controller.interfaces.VagaInterfaceController;
+import br.com.allocation.dto.pageDTO.PageDTO;
+import br.com.allocation.dto.vagaDTO.VagaCreateDTO;
+import br.com.allocation.dto.vagaDTO.VagaDTO;
+import br.com.allocation.exceptions.RegraDeNegocioException;
+import br.com.allocation.service.VagaService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/vaga")
+@Validated
+@RequiredArgsConstructor
+@Slf4j
+public class VagaController implements VagaInterfaceController {
+
+    private final VagaService vagaService;
+
+    @Override
+    public ResponseEntity<VagaDTO> salvar(VagaCreateDTO vagaCreateDTO) throws RegraDeNegocioException {
+
+        log.info("Adicionando a vaga...");
+        VagaDTO vaga = vagaService.salvar(vagaCreateDTO);
+        log.info("Vaga adicionado com sucesso!");
+        return new ResponseEntity<>(vaga, HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<PageDTO<VagaDTO>> listar(Integer pagina, Integer tamanho) {
+        return ResponseEntity.ok(vagaService.listar(pagina, tamanho));
+    }
+
+    @Override
+    public ResponseEntity<VagaDTO> editar(Integer id, VagaCreateDTO vagaCreateDTO) throws RegraDeNegocioException {
+
+        log.info("Editando a vaga...");
+        VagaDTO vaga = vagaService.editar(id, vagaCreateDTO);
+        log.info("Vaga editada com sucesso!");
+        return new ResponseEntity<>(vaga, HttpStatus.CREATED);
+    }
+
+    @Override
+    public ResponseEntity<Void> deletar(Integer id) throws RegraDeNegocioException {
+        vagaService.deletar(id);
+        log.info("Vaga deletado com sucesso");
+        return ResponseEntity.noContent().build();
+    }
+}
--- src/main/java/br/com/allocation/controller/interfaces/VagaInterfaceController.java
+++ src/main/java/br/com/allocation/controller/interfaces/VagaInterfaceController.java
@@ -1,4 +1,65 @@
 package br.com.allocation.controller.interfaces;
 
+import br.com.allocation.dto.pageDTO.PageDTO;
+import br.com.allocation.dto.vagaDTO.VagaCreateDTO;
+import br.com.allocation.dto.vagaDTO.VagaDTO;
+import br.com.allocation.exceptions.RegraDeNegocioException;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
 public interface VagaInterfaceController {
+
+    @Operation(summary = "Criar um registro de vaga.", description = "Cria um cadastro de vaga no banco de dados.")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Cria vaga."),
+                    @ApiResponse(responseCode = "200", description = "recupera dados do vaga logado no banco de dados."),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @PostMapping
+    ResponseEntity<VagaDTO> salvar(
+            @RequestBody @Valid VagaCreateDTO vagaCreateDTO)
+            throws RegraDeNegocioException;
+
+
+    @Operation(summary = "Listar todos as vagas", description = "Listar todos as vagas")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Listar todos as vagas do banco"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @GetMapping("/listAllUsers")
+    ResponseEntity<PageDTO<VagaDTO>> listar(Integer pagina, Integer tamanho);
+
+    @Operation(summary = "Editar a vaga por id", description = "Editar vaga por id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Editar a vaga no banco de dados"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @PutMapping("/editar")
+    ResponseEntity<VagaDTO> editar(Integer id, VagaCreateDTO vagaCreateDTO) throws RegraDeNegocioException;
+
+
+    @Operation(summary = "Deleta a vaga por id", description = "Deleta vaga por id")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Deleta a vaga do banco de dados"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
+    @DeleteMapping("/deletar/{id}")
+    ResponseEntity<Void> deletar(Integer id) throws RegraDeNegocioException;
 }
