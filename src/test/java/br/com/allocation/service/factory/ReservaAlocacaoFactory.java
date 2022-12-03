e098c28e07f6e421de57e72acf89cdd6e9418a14
Kaio Antonio <kaioaar@gmail.com>
2022-12-03 11:47:24 -0300

    fix: erro na factory de alocacao

--- src/test/java/br/com/allocation/service/factory/ReservaAlocacaoFactory.java
+++ src/test/java/br/com/allocation/service/factory/ReservaAlocacaoFactory.java
@@ -9,6 +9,7 @@ import java.time.LocalDate;
 public class ReservaAlocacaoFactory {
     public static ReservaAlocacaoEntity getReservaAlocacaoEntity() {
         ReservaAlocacaoEntity reservaAlocacaoEntity = new ReservaAlocacaoEntity(1,
+                2,
                 "xx",
                 LocalDate.now().plusDays(2),
                 LocalDate.now().plusMonths(1),
