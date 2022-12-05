61be68e969ce82b0bb65f77292d56036e6a98329
Kaio Antônio <kaioaar@gmail.com>
2022-12-05 10:47:33 -0300

    versao estabilizada

--- src/main/java/br/com/allocation/dto/programaDTO/ProgramaDTO.java
+++ src/main/java/br/com/allocation/dto/programaDTO/ProgramaDTO.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDate;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -13,4 +15,5 @@ public class ProgramaDTO {
     private String nome;
     private String descricao;
     private String situacao;
+    private LocalDate dataTermino;
 }
